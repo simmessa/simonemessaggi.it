7b61168b91842eefdb8c747a4cdea9a398f074cf
Simone Messaggi <simone.messaggi@mia-platform.eu>
2020-12-09 18:50:19 +0100

    fixed siteurl

--- dev.simonemessaggi.it/data/SiteConfig.js
+++ dev.simonemessaggi.it/data/SiteConfig.js
@@ -3,7 +3,7 @@ const config = {
   siteTitleShort: 'Simone Messaggi', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: 'Simone Messaggi', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://dev.simonemessaggi.it', // Domain of your website without pathPrefix.
+  siteUrl: 'https://simonemessaggi.it', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription:
     'A DevOps Journey.', // Website description used for RSS feeds/meta description tag.
